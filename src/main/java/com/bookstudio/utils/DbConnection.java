e7321da557ab01ab426303ad91ef67af5e13f8bd
jason-vila <jasonvilac@gmail.com>
2025-03-03 12:42:42 -0500

    chore: update DbConnection class to remove hardcoded credentials and change default user/pass

--- src/main/java/com/bookstudio/utils/DbConnection.java
+++ src/main/java/com/bookstudio/utils/DbConnection.java
@@ -8,7 +8,7 @@ public class DbConnection {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost/bookstudio_db?useSSL=false&useTimezone=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "rootmysql";
+    private static final String PASSWORD = "your_password";
     
     public static Connection getConexion() {
         Connection con = null;
